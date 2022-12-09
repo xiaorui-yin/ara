2ed5dc479b3a4ce3b91c5ee64d6f3ad51281b834
Xiaorui Yin (yinx) <msc22h3@badile20.ee.ethz.ch>
2022-12-09 16:44:49 +0100

    Fix vmfpu state transistion, delete redundant files, update license

--- hardware/include/matmul_pkg.sv
+++
@@ -1,9 +0,0 @@
-// Author: Xiaorui Yin
-
-package matmul_pkg;
-  // =================================================================
-  // Parameter
-  // =================================================================
-
-  // Maximum size of the broadcast vector 
-  localparam int unsigned MAX_BLEN = `ifdef MAX_BLEN `MAX_BLEN `else 64 `endif;
