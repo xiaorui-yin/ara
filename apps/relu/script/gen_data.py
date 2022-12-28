Fix multihead attention algorithm, and correct all performance calculation

apps/relu/script/gen_data.py
@@ -40,7 +40,7 @@ def relu(mat):
     act = nn.ReLU()
     return act(torch.from_numpy(mat)).numpy().astype(np.float32)
 
-(row, col) = (64, 768)
+(row, col) = (64, 1280)
 
 # Generate inputs
 mat = rand_matrix(row, col)